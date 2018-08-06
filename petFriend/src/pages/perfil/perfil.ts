48c576f7018c9abf32e59cd67fc93ebb7777655b
jacquelineStella <31634683+jacquelineStella@users.noreply.github.com>
2018-08-06 13:34:39 -0300

    se vuelve a cargar los modulos de perfil

--- petFriend/src/pages/perfil/perfil.ts
+++ petFriend/src/pages/perfil/perfil.ts
@@ -20,6 +20,8 @@ export class PerfilPage {
   id: '';
   telefono:' ';
   nombre:'';
+  contactoNombre:any;  
+  p: any;
   constructor(public navCtrl: NavController, public navParams: NavParams,private db :DbProvider) {
       
   }
@@ -30,9 +32,13 @@ export class PerfilPage {
   }
   ionViewDidEnter(){   
     this.db.getPerfil().subscribe(perfil=>{
-      this.perfil = perfil;
-    })
-  }
+      this.p = perfil;
+      console.log(this.p);
+      this.contactoNombre=this.p[0];
+      this.telefono=this.p[1];
+      console.log(this.contactoNombre)
+         })
+        }
 
 }
 
