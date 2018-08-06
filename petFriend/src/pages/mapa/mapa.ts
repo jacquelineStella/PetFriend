36b750e549898bfcc0b6b30711e6f8a8ac4d9fe9
jacquelineStella <31634683+jacquelineStella@users.noreply.github.com>
2018-08-06 16:02:13 -0300

    modulo roto de mapa

--- petFriend/src/pages/mapa/mapa.ts
+++ petFriend/src/pages/mapa/mapa.ts
@@ -1,9 +1,8 @@
-import { Component , NgZone} from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Platform } from 'ionic-angular';
 import { DbProvider } from '../../providers/db/db';
-import { LoadingController } from 'ionic-angular';
 declare var google: any; 
 /**
  * Generated class for the MapaPage page.
@@ -18,134 +17,32 @@ declare var google: any;
   templateUrl: 'mapa.html',
 })
 export class MapaPage {
-  map: any;
-  markers: any;
-  autocomplete: any;
-  GoogleAutocomplete: any;
-  GooglePlaces: any;
-  geocoder: any
-  autocompleteItems: any;
-  loading: any;
+  map:any; //Manejador Mapa
+  coords: any={lat:0, lng:0} //coordenadas
+  infowindow;
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
-     public  platform: Platform,     
-     public zone: NgZone,
-     public geolocation: Geolocation,
-     public loadingCtrl: LoadingController) {
-  
-  
-      this.geocoder = new google.maps.Geocoder;
-      let elem = document.createElement("div")
-      this.GooglePlaces = new google.maps.places.PlacesService(elem);
-      this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
-      this.autocomplete = {
-        input: ''
-      };
-      this.autocompleteItems = [];
-      this.markers = [];
-      this.loading = this.loadingCtrl.create();
-    }
-  
-    ionViewDidEnter(){
-        // let infoWindow = new google.maps.InfoWindow({map: map});
-        //Set latitude and longitude of some place
-      this.map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.9011, lng: -56.1645},
-        zoom: 15
-      });
-    }
-  
-    tryGeolocation(){
-      this.loading.present();
-      this.clearMarkers();//remove previous markers
-      this.geolocation.getCurrentPosition().then((resp) => {
-        let pos = {
-          lat: resp.coords.latitude,
-          lng: resp.coords.longitude
-        };
-        let marker = new google.maps.Marker({
-          position: pos,
-          map: this.map,
-          title: 'I am here!'
-        });
-        this.markers.push(marker);
-        this.map.setCenter(pos);
-        this.loading.dismiss();
-  
-      }).catch((error) => {
-        console.log('Error getting location', error);
-        this.loading.dismiss();
-      });
-    }
-  
-    updateSearchResults(){
-      if (this.autocomplete.input == '') {
-        this.autocompleteItems = [];
-        return;
-      }
-      this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
-        (predictions, status) => {
-          this.autocompleteItems = [];
-          if(predictions){
-            this.zone.run(() => {
-              predictions.forEach((prediction) => {
-                this.autocompleteItems.push(prediction);
-              });
-            });
-          }
-      });
-    }
-  
-    selectSearchResult(item){
-      this.clearMarkers();
-      this.autocompleteItems = [];
-  
-      this.geocoder.geocode({'placeId': item.place_id}, (results, status) => {
-        if(status === 'OK' && results[0]){
-          // let position = {
-          //     lat: results[0].geometry.location.lat,
-          //     lng: results[0].geometry.location.lng
-          // };
-          let marker = new google.maps.Marker({
-            position: results[0].geometry.location,
-            map: this.map
-          });
-          this.markers.push(marker);
-          this.map.setCenter(results[0].geometry.location);
-        }
-      })
-    }
-  
-    clearMarkers(){
-      for (var i = 0; i < this.markers.length; i++) {
-        console.log(this.markers[i])
-        this.markers[i].setMap(null);
-      }
-      this.markers = [];
-    }
-  
+     public  platform: Platform,
+     private geolocation: Geolocation,
+     private db :DbProvider,) {
+       //obtener posicion del usuario
+     platform.ready().then(()=>{
+      this.obtenerPosicion();
+   
+   });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapaPage');
   }
-}
-
-  // map:any; //Manejador Mapa
-  // coords: any={lat:0, lng:0} //coordenadas
-
-  //      //obtener posicion del usuario
-  //    platform.ready().then(()=>{
-  //     this.obtenerPosicion();
-   
-  //  });
-  // loadMap(){
-  //   let mapContainer=document.getElementById('map');
-  //     this.map=new google.maps.Map(mapContainer,{
-  //       center: this.coords
-  //       ,zoom: 12
-  //     });
-  //     this.marcador(this.map, this.coords );
+  loadMap(){
+    let mapContainer=document.getElementById('map');
+      this.map=new google.maps.Map(mapContainer,{
+        center: this.coords
+        ,zoom: 12
+      });
+      this.marcador(this.map, this.coords );
       //This.referencia(this.map,this.coords )
       
     
@@ -161,7 +58,7 @@ export class MapaPage {
   //   types: ['cafe']
   // };
 
-  
+  }
   // referencia(map,posicion){
   //     let referencia= new google.maps.referencia({
   //       location: posicion,
@@ -172,32 +69,32 @@ export class MapaPage {
 
   // }
 
-  // marcador( map, position){
-  //   let miMarker = new google.maps.Marker({
-  //     icon : 'assets/imgs/ico_estoy_aqui.png',
-  //     map: map,
-  //     position: position
-  //   });
-  // }
+  marcador( map, position){
+    let miMarker = new google.maps.Marker({
+      icon : 'assets/imgs/ico_estoy_aqui.png',
+      map: map,
+      position: position
+    });
+  }
 
 
-  // obtenerPosicion(): any{
-  //   let locationOptions = { timeout: 30000, enableHighAccuracy: true };
-  //   this.geolocation.getCurrentPosition(locationOptions).then(res=>{
-  //     this.coords.lat= res.coords.latitude;
-  //     this.coords.lng=res.coords.longitude;
-  //     this.loadMap();
-  //   })
-  //   .catch(
+  obtenerPosicion(): any{
+    let locationOptions = { timeout: 30000, enableHighAccuracy: true };
+    this.geolocation.getCurrentPosition(locationOptions).then(res=>{
+      this.coords.lat= res.coords.latitude;
+      this.coords.lng=res.coords.longitude;
+      this.loadMap();
+    })
+    .catch(
 
-  //     (error)=>{
+      (error)=>{
        
-  //       console.log(error);        
-  //     }
-  //     );
+        console.log(error);        
+      }
+      );
     
 
-  //   }
+    }
 
 
-//}
+}
