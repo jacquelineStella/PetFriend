adcaa59f223763fe685999e681f9ac390a9a629d
jacquelineStella <31634683+jacquelineStella@users.noreply.github.com>
2018-08-06 13:46:03 -0300

    se vuelve a cargar modulo principal roto

--- petFriend/src/pages/mapa/mapa.ts
+++ petFriend/src/pages/mapa/mapa.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Geolocation } from '@ionic-native/geolocation';
+import { Platform } from 'ionic-angular';
+import { DbProvider } from '../../providers/db/db';
+declare var google: any; 
 /**
  * Generated class for the MapaPage page.
  *
@@ -14,12 +17,83 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'mapa.html',
 })
 export class MapaPage {
+  map:any; //Manejador Mapa
+  coords: any={lat:0, lng:0} //coordenadas
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+     public  platform: Platform,
+     private geolocation: Geolocation,
+     private db :DbProvider,) {
+       //obtener posicion del usuario
+     platform.ready().then(()=>{
+      this.obtenerPosicion();
+   
+   });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapaPage');
   }
+  loadMap(){
+    let mapContainer=document.getElementById('map');
+      this.map=new google.maps.Map(mapContainer,{
+        center: this.coords
+        ,zoom: 12
+      });
+      this.marcador(this.map, this.coords );
+      //This.referencia(this.map,this.coords )
+      
+    
+             // Colocamos el marcador
+  //   let miMarker = new google.maps.Marker({
+  //     icon : 'assets/imgs/ico_estoy_aqui.png',
+  //     map: this.map,
+  //     position: this.coords
+  // });
+  // let request = {
+  //   location: myLatlng,
+  //   radius: 5000,
+  //   types: ['cafe']
+  // };
+
+  }
+  // referencia(map,posicion){
+  //     let referencia= new google.maps.referencia({
+  //       location: posicion,
+  //       map:this.map,
+  //       radius:2000,
+  //       types:['veterinaria']
+  //     });
+
+  // }
+
+  marcador( map, position){
+    let miMarker = new google.maps.Marker({
+      icon : 'assets/imgs/ico_estoy_aqui.png',
+      map: map,
+      position: position
+    });
+  }
+
+
+  obtenerPosicion(): any{
+    let locationOptions = { timeout: 30000, enableHighAccuracy: true };
+    this.geolocation.getCurrentPosition(locationOptions).then(res=>{
+      this.coords.lat= res.coords.latitude;
+      this.coords.lng=res.coords.longitude;
+      this.loadMap();
+    })
+    .catch(
+
+      (error)=>{
+       
+        console.log(error);        
+      }
+      );
+    
+
+    }
+
 
 }
--- petFriend/src/pages/principal/principal.ts
+++ petFriend/src/pages/principal/principal.ts
@@ -4,11 +4,10 @@ import { AuthProvider } from '../../providers/auth/auth';
 import { PublicacionesPage } from '../publicaciones/publicaciones';
 import {AdopcionesPage} from '../adopciones/adopciones';
 import {EncontradosPage} from '../encontrados/encontrados'
+import {MapaPage} from '../mapa/mapa';
 
-import { Geolocation } from '@ionic-native/geolocation';
 import { Platform } from 'ionic-angular';
-import { DbProvider } from '../../providers/db/db';
-declare var google: any; 
+
 /**
  * Generated class for the PrincipalPage page.
  *
@@ -22,24 +21,18 @@ declare var google: any;
   templateUrl: 'principal.html',
 })
 export class PrincipalPage {
-  map:any; //Manejador Mapa
-  coords: any={lat:0, lng:0} //coordenadas
+ 
   
   
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
      public auth : AuthProvider,
      public menuCtrl: MenuController ,
-     public  platform: Platform,
-     private geolocation: Geolocation,
-     private db :DbProvider,
+     public  platform: Platform
+    
     ) {
      this.menuCtrl.enable(true); 
-     //obtener posicion del usuario
-     platform.ready().then(()=>{
-        this.obtenerPosicion();
-     
-     });
+    
      
   }
 
@@ -56,72 +49,14 @@ export class PrincipalPage {
   }
 
   irMapa(){
-    
+    this.navCtrl.push(MapaPage);
   }
 
-  loadMap(){
-    let mapContainer=document.getElementById('map');
-      this.map=new google.maps.Map(mapContainer,{
-        center: this.coords
-        ,zoom: 12
-      });
-      this.marcador(this.map, this.coords );
-      //This.referencia(this.map,this.coords )
-      
-    
-             // Colocamos el marcador
-  //   let miMarker = new google.maps.Marker({
-  //     icon : 'assets/imgs/ico_estoy_aqui.png',
-  //     map: this.map,
-  //     position: this.coords
-  // });
-  // let request = {
-  //   location: myLatlng,
-  //   radius: 5000,
-  //   types: ['cafe']
-  // };
-
-  }
-  // referencia(map,posicion){
-  //     let referencia= new google.maps.referencia({
-  //       location: posicion,
-  //       map:this.map,
-  //       radius:2000,
-  //       types:['veterinaria']
-  //     });
-
-  // }
-
-  marcador( map, position){
-    let miMarker = new google.maps.Marker({
-      icon : 'assets/imgs/ico_estoy_aqui.png',
-      map: map,
-      position: position
-    });
-  }
-
-
-  obtenerPosicion(): any{
-    let locationOptions = { timeout: 30000, enableHighAccuracy: true };
-    this.geolocation.getCurrentPosition(locationOptions).then(res=>{
-      this.coords.lat= res.coords.latitude;
-      this.coords.lng=res.coords.longitude;
-      this.loadMap();
-    })
-    .catch(
-
-      (error)=>{
-       
-        console.log(error);        
-      }
-      );
-    
-
-    }
+}
 
 
 
  
 
 
-}
+
